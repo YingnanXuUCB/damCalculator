266c5f93fb09036b22b16459bec876eb907a82a4
YingnanXuUCB <70857440+YingnanXuUCB@users.noreply.github.com>
2020-09-07 03:09:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="YingnanXuUCB", # Replace with your own username
+    name="damcalculator", # Replace with your own username
     version="0.0.1",
     author="YingnanXu",
     author_email="yingnanxu@berkeley.edu",
